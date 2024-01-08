96cca29ed3c8c09836747d7bd6305903b28771a8
viktormuller <viktormuller@gmail.com>
2024-01-08 19:17:43 +0000

    Remove parameter logging

--- source/train.py
+++ source/train.py
@@ -41,7 +41,6 @@ def test(model, test_loader, device):
     )
 
 import zipfile
-import itertools
 
 def train(args):
     use_cuda = args.num_gpus > 0
@@ -94,9 +93,6 @@ def train(args):
                     ),
                     flush=True
                 )
-                for name, param in itertools.islice(network.named_parameters(), 10):
-                    if param.requires_grad:
-                        print(name, param.data)
         test(network, usc_test_dataloader, device)
         scheduler.step()
     save_model(network, args.model_dir)
